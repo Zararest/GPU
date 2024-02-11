184fafdb208c5d546c0d3b83036a31133f8db880
UwU <iishumakov@gmail.com>
2024-02-11 21:46:02 +0300

    PBQP beginning

---
+++ PBQP/include/Entities.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cstddef>
+#include <vector>
+#include <string>
+
+namespace SSA {
+
+class Value {
+  size_t ID;
+  size_t Type;
+};
+
+class Instruction {
+  Value Def;
+  std::vector<Value> Use;
+  std::string Name;
+};
+
+} // namespace SSA
+
+namespace MIR {
+  
+class Register final {
+  size_t ID;
+  size_t Type;
+};
+
+class Instruction {
+  Register Def;
+  std::vector<Register> Use;
+  std::string Name;
+
+public:
+};
+
+class Spill final : public Instruction {};
+class Fill final : public Instruction {};
+
+} // namespace MIR 
